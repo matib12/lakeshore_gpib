a70294cbe2b1b5ca0634ba1e3d06ecbc9e7e2eac
Mateusz Bawaj <mateusz.bawaj@unipg.it>
2023-06-14 19:28:23 +0900

    Flush monitor.py modifications.

--- monitor.py
+++ monitor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+import schedule
+import time
 import asyncio
 from prologix_gpib_async import AsyncPrologixGpibEthernetController
 
-IPaddr = '10.42.0.64'
-GPIBaddr = 21
+IPaddr = '192.168.11.13'
+GPIBaddr = 12
 
 async def init():
     try: 
@@ -39,11 +41,11 @@ async def read_temp(chan):
     
     try: 
         async with AsyncPrologixGpibEthernetController(IPaddr, pad=GPIBaddr) as gpib_device:
-            gpib_device.write("CRDG? ")
+            await gpib_device.write(("CRDG? " + chan).encode(encoding='UTF-8'))
             # Instruct the controller to read until the device sets <EOI>, then read until '\n'
             # from the prologix controller
-            th_couple_T = gpib_device.read()
-            print("Thermocouple Junction Temperature:", th_couple_T)
+            T = await gpib_device.read()
+            print("Thermocouple Junction Temperature:", T)
     except (ConnectionError, ConnectionRefusedError):
         print("Could not connect to remote target. Is the device connected?")
 
@@ -51,5 +53,13 @@ async def read_temp(chan):
 async def main():
     await init()
 
+    for i in range(2):
+        await read_temp('A')
+        time.sleep(3)
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    schedule.every(1).minutes.do(asyncio.run(main()))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
