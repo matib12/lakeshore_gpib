01ca180a4c38e3922f952811718748ef24db92d0
Mateusz Bawaj <mateusz.bawaj@unipg.it>
2023-06-21 22:25:29 +0900

    Added direct writing to InfluxDB.

--- lakeshore_read.py
+++ lakeshore_read.py
@@ -5,12 +5,28 @@ import datetime
 import asyncio
 from prologix_gpib_async import AsyncPrologixGpibEthernetController
 
-IPaddr = '192.168.11.13'
+prologixIPaddr = '192.168.11.13'
 GPIBaddr = 12
+
+# InfluxDB server
+token = "dqaJw6NXj1otMUE8MctTqQhf7cn8kZue46CG04nKTS9NW-Fw9T9qpsZmAOg3N-EmIx17YRWBcLfNLFBmlT24vg=="
+org = "Unipg"
+#url = "http://172.16.35.113:8086"
+url = "http://192.168.11.21:8086"
+
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+bucket="ICRR_test"
+
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
 async def init():
     try: 
-        async with AsyncPrologixGpibEthernetController(IPaddr, pad=GPIBaddr) as gpib_device:
+        async with AsyncPrologixGpibEthernetController(prologixIPaddr, pad=GPIBaddr) as gpib_device:
             version = await gpib_device.version()
             print("Controller version:", version)
             
@@ -25,7 +41,7 @@ async def init():
 
 async def read_room_temp():
     try: 
-        async with AsyncPrologixGpibEthernetController(IPaddr, pad=GPIBaddr) as gpib_device:
+        async with AsyncPrologixGpibEthernetController(prologixIPaddr, pad=GPIBaddr) as gpib_device:
             gpib_device.write("TEMP?")
             # Instruct the controller to read until the device sets <EOI>, then read until '\n'
             # from the prologix controller
@@ -40,7 +56,7 @@ async def read_temp(chan):
         return
     
     try: 
-        async with AsyncPrologixGpibEthernetController(IPaddr, pad=GPIBaddr) as gpib_device:
+        async with AsyncPrologixGpibEthernetController(prologixIPaddr, pad=GPIBaddr) as gpib_device:
             await gpib_device.write(("CRDG? " + chan).encode(encoding='UTF-8'))
             # Instruct the controller to read until the device sets <EOI>, then read until '\n'
             # from the prologix controller
@@ -48,6 +64,16 @@ async def read_temp(chan):
             return T
     except (ConnectionError, ConnectionRefusedError):
         print("Could not connect to remote target. Is the device connected?")
+
+def write_influx(ta, tb, time):
+    point = (Point("temp")
+             .tag("unit", "C")
+             .field("t_A", float(ta))
+             .field("t_B", float(tb))
+             .time(time)
+            )
+
+    write_api.write(bucket=bucket, org="Unipg", record=point)
 
 
 async def main():
@@ -68,7 +94,9 @@ async def main():
 
     current_time = n.isoformat()
 
-    print(current_time + "\t{:.2f}\t{:.2f}".format(T_A, T_B))
+    write_influx(T_A, T_B, current_time)
+
+    #print(current_time + "\t{:.2f}\t{:.2f}".format(T_A, T_B))
 
 
 if __name__ == "__main__":
